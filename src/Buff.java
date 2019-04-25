53c9a4a5ec99a05c661825c6d521d73403718846
kasraabdollahi <kasra.abdolahi1379@gmail.com>
2019-04-25 18:58:16 +0430

    almost redesigned Buffs

--- src/Buff.java
+++ src/Buff.java
@@ -1,2 +1,38 @@
-public abstract class Buff {
+public class Buff {
+    private int changeHp;
+    private int changeAp;
+    private int numberOfTurns;
+    private int delay;
+    private boolean isDisarmer;
+    private String targetType;
+    private int[] x;
+    private int[] y;
+
+    public int getChangeAp() {
+        return changeAp;
+    }
+
+    public int getChangeHp() {
+        return changeHp;
+    }
+
+    public int getDelay() {
+        return delay;
+    }
+
+    public int getNumberOfTurns() {
+        return numberOfTurns;
+    }
+
+    public int[] getX() {
+        return x;
+    }
+
+    public int[] getY() {
+        return y;
+    }
+
+    public void nutralize(){
+
+    }
 }
