048747e48463cbe06e8d5ae186c7e647273fca03
mhgheisarieh <mgheysariyeh@gmail.com>
2019-04-28 21:24:48 +0430

    add some getter and setter in card , intiallize the deck class

--- src/Card.java
+++ src/Card.java
@@ -3,4 +3,36 @@ public abstract class Card {
     protected String name;
     protected int requiredManas;
     protected int price;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRequiredManas() {
+        return requiredManas;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRequiredManas(int requiredManas) {
+        this.requiredManas = requiredManas;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
--- src/Deck.java
+++ src/Deck.java
@@ -1,2 +1,33 @@
+import java.util.HashMap;
+
 public class Deck {
+    private String name;
+    private HashMap<String, Card> cards;
+
+    public Deck(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public HashMap<String, Card> getCards() {
+        return cards;
+    }
+
+    public void addCard(Card card) {
+        this.getCards().put(card.getName(), card);
+    }
+
+    private int getNumOfOtherHeroCards() {
+        int i = 0;
+        for (Card card : this.getCards().values()) {
+            if (!(card instanceof Hero)) {
+                i++;
+            }
+        }
+        return i;
+    }
+
 }
