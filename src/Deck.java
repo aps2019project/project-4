aaf18970dd0dc07360f4ce8e86d2aa401272e7bf
mhgheisarieh <mgheysariyeh@gmail.com>
2019-04-28 23:48:05 +0430

    add validate deck

--- src/Deck.java
+++ src/Deck.java
@@ -3,9 +3,12 @@ import java.util.HashMap;
 public class Deck {
     private String name;
     private HashMap<String, Card> cards;
+    private UsabelItem item;
 
     public Deck(String name) {
         this.name = name;
+        this.cards = new HashMap<>();
+        this.item = null;
     }
 
     public String getName() {
@@ -14,6 +17,10 @@ public class Deck {
 
     public HashMap<String, Card> getCards() {
         return cards;
+    }
+
+    public void setItem(UsabelItem item) {
+        this.item = item;
     }
 
     public void addCard(Card card) {
@@ -39,4 +46,9 @@ public class Deck {
         return false;
     }
 
+    public boolean validateDeck(){
+        if (this.isHaveHero() & this.getNumOfOtherHeroCards() == 20)
+            return true;
+        return false;
+    }
 }
