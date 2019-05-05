b7ccfab66b60db009ab32f3d8ea43431246b2f9a
kasraabdollahi <kasra.abdolahi1379@gmail.com>
2019-05-05 23:54:39 +0430

    5 first minions added to minion resources

--- src/resources/MinionResources.java
+++ src/resources/MinionResources.java
@@ -27,9 +27,9 @@ public class MinionResources {
         return getMinionResource().get(minionName);
     }
 
-    public static ArrayList<Minion> getAllMinions(){
-        ArrayList <Minion> minions = new ArrayList<>();
-        getMinionResource().forEach((name , gson) -> minions.add(getSpecificMinion(name)));
+    public static ArrayList<Minion> getAllMinions() {
+        ArrayList<Minion> minions = new ArrayList<>();
+        getMinionResource().forEach((name, gson) -> minions.add(getSpecificMinion(name)));
         return minions;
     }
 
@@ -52,5 +52,226 @@ public class MinionResources {
         //getMinionResource().put("persianSwordman", "");
         //todo add other minion Gsons
     }
+
+    String persianArcher = "{\n" +
+            "\"healthPoint\": 6,\n" +
+            "\"attackPoint\": 4,\n" +
+            "\"startRange\": 2,\n" +
+            "\"endRange\": 7,\n" +
+            "\"isAntiHolyBuff\": false,\n" +
+            "\"isAntiDisarm\": false,\n" +
+            "\"isAntiPoison\": false,\n" +
+            "\"isImmortal\": false,\n" +
+            "\"isApSuperior\": false,\n" +
+            "\"type\": \"RANGED\",\n" +
+            "\"hasAttackedThisTurn\": false,\n" +
+            "\"hasMovedThisTurn\": false,\n" +
+            "\"isDisarmed\": false,\n" +
+            "\"isStunned\": false,\n" +
+            "\"numberOfTurnsOfDisarm\": 0,\n" +
+            "\"numberOfTurnsOfStun\": 0,\n" +
+            "\"specialPowerActivationType\": \"NONE\",\n" +
+            "\"positiveBuffs\": [],\n" +
+            "\"negativeBuffs\": [],\n" +
+            "\"hasFlag\": false,\n" +
+            "\"id\": \"\",\n" +
+            "\"name\": \"persian archer\",\n" +
+            "\"requiredManas\": 2,\n" +
+            "\"price\": 300\n" +
+            "}";
+    String persianSwordsman = "{\n" +
+            "\"healthPoint\": 6,\n" +
+            "\"attackPoint\": 4,\n" +
+            "\"startRange\": 1,\n" +
+            "\"endRange\": 1,\n" +
+            "\"isAntiHolyBuff\": false,\n" +
+            "\"isAntiDisarm\": false,\n" +
+            "\"isAntiPoison\": false,\n" +
+            "\"isImmortal\": false,\n" +
+            "\"isApSuperior\": false,\n" +
+            "\"type\": \"MELEE\",\n" +
+            "\"hasAttackedThisTurn\": false,\n" +
+            "\"hasMovedThisTurn\": false,\n" +
+            "\"isDisarmed\": false,\n" +
+            "\"isStunned\": false,\n" +
+            "\"numberOfTurnsOfDisarm\": 0,\n" +
+            "\"numberOfTurnsOfStun\": 0,\n" +
+            "\"specialPower\": {\n" +
+            "\"cellOrSoldier\": \"SOLDIER\",\n" +
+            "\"target\": \"ENEMY\",\n" +
+            "\"cellsType\": \"MONO_CELL\",\n" +
+            "\"lengthOfSideOfSquare\": 0,\n" +
+            "\"buffNutralizer\": \"NONE\",\n" +
+            "\"buffs\": [\n" +
+            "{\n" +
+            "\"numberOfTurnsOnTarget\": 0,\n" +
+            "\"changeHp\": 0,\n" +
+            "\"apShield\": 0,\n" +
+            "\"changeAp\": 0,\n" +
+            "\"numberOfTurns\": 1,\n" +
+            "\"delay\": 0,\n" +
+            "\"isDisarmer\": false,\n" +
+            "\"isStunner\": true,\n" +
+            "\"isPositive\": false,\n" +
+            "\"onCellOrSoldier\": \"SOLDIER\",\n" +
+            "\"isActive\": false\n" +
+            "}\n" +
+            "],\n" +
+            "\"cells\": [],\n" +
+            "\"id\": \"\",\n" +
+            "\"name\": \"persian swordsman spell\",\n" +
+            "\"requiredManas\": 2,\n" +
+            "\"price\": 0\n" +
+            "},\n" +
+            "\"specialPowerActivationType\": \"ON_ATTACK\",\n" +
+            "\"positiveBuffs\": [],\n" +
+            "\"negativeBuffs\": [],\n" +
+            "\"hasFlag\": false,\n" +
+            "\"id\": \"\",\n" +
+            "\"name\": \"persian swordsman\",\n" +
+            "\"requiredManas\": 2,\n" +
+            "\"price\": 400\n" +
+            "}";
+    String persianSpearsman = "{\n" +
+            "\"healthPoint\": 5,\n" +
+            "\"attackPoint\": 3,\n" +
+            "\"startRange\": 1,\n" +
+            "\"endRange\": 3,\n" +
+            "\"isAntiHolyBuff\": false,\n" +
+            "\"isAntiDisarm\": false,\n" +
+            "\"isAntiPoison\": false,\n" +
+            "\"isImmortal\": false,\n" +
+            "\"isApSuperior\": false,\n" +
+            "\"type\": \"HYBRID\",\n" +
+            "\"hasAttackedThisTurn\": false,\n" +
+            "\"hasMovedThisTurn\": false,\n" +
+            "\"isDisarmed\": false,\n" +
+            "\"isStunned\": false,\n" +
+            "\"numberOfTurnsOfDisarm\": 0,\n" +
+            "\"numberOfTurnsOfStun\": 0,\n" +
+            "\"specialPowerActivationType\": \"NONE\",\n" +
+            "\"positiveBuffs\": [],\n" +
+            "\"negativeBuffs\": [],\n" +
+            "\"hasFlag\": false,\n" +
+            "\"id\": \"\",\n" +
+            "\"name\": \"persian spearsman\",\n" +
+            "\"requiredManas\": 1,\n" +
+            "\"price\": 500\n" +
+            "}";
+    String persianHorseman = "{\n" +
+            "\"healthPoint\": 10,\n" +
+            "\"attackPoint\": 6,\n" +
+            "\"startRange\": 1,\n" +
+            "\"endRange\": 1,\n" +
+            "\"isAntiHolyBuff\": false,\n" +
+            "\"isAntiDisarm\": false,\n" +
+            "\"isAntiPoison\": false,\n" +
+            "\"isImmortal\": false,\n" +
+            "\"isApSuperior\": false,\n" +
+            "\"type\": \"MELEE\",\n" +
+            "\"hasAttackedThisTurn\": false,\n" +
+            "\"hasMovedThisTurn\": false,\n" +
+            "\"isDisarmed\": false,\n" +
+            "\"isStunned\": false,\n" +
+            "\"numberOfTurnsOfDisarm\": 0,\n" +
+            "\"numberOfTurnsOfStun\": 0,\n" +
+            "\"specialPowerActivationType\": \"NONE\",\n" +
+            "\"positiveBuffs\": [],\n" +
+            "\"negativeBuffs\": [],\n" +
+            "\"hasFlag\": false,\n" +
+            "\"id\": \"\",\n" +
+            "\"name\": \"persian horseman\",\n" +
+            "\"requiredManas\": 4,\n" +
+            "\"price\": 200\n" +
+            "}";
+    String persianChampion = "{\n" +
+            "\"healthPoint\": 24,\n" +
+            "\"attackPoint\": 6,\n" +
+            "\"startRange\": 1,\n" +
+            "\"endRange\": 1,\n" +
+            "\"isAntiHolyBuff\": false,\n" +
+            "\"isAntiDisarm\": false,\n" +
+            "\"isAntiPoison\": false,\n" +
+            "\"isImmortal\": false,\n" +
+            "\"isApSuperior\": false,\n" +
+            "\"type\": \"MELEE\",\n" +
+            "\"hasAttackedThisTurn\": false,\n" +
+            "\"hasMovedThisTurn\": false,\n" +
+            "\"isDisarmed\": false,\n" +
+            "\"isStunned\": false,\n" +
+            "\"numberOfTurnsOfDisarm\": 0,\n" +
+            "\"numberOfTurnsOfStun\": 0,\n" +
+            "\"specialPower\": {\n" +
+            "\"cellOrSoldier\": \"SOLDIER\",\n" +
+            "\"target\": \"ENEMY\",\n" +
+            "\"cellsType\": \"MONO_CELL\",\n" +
+            "\"lengthOfSideOfSquare\": 0,\n" +
+            "\"buffNutralizer\": \"NONE\",\n" +
+            "\"buffs\": [\n" +
+            "{\n" +
+            "\"numberOfTurnsOnTarget\": 0,\n" +
+            "\"changeHp\": 0,\n" +
+            "\"apShield\": 0,\n" +
+            "\"changeAp\": 5,\n" +
+            "\"numberOfTurns\": 2147483647,\n" +
+            "\"delay\": 0,\n" +
+            "\"isDisarmer\": false,\n" +
+            "\"isStunner\": false,\n" +
+            "\"isPositive\": true,\n" +
+            "\"onCellOrSoldier\": \"SOLDIER\",\n" +
+            "\"isActive\": false\n" +
+            "}\n" +
+            "],\n" +
+            "\"cells\": [],\n" +
+            "\"id\": \"\",\n" +
+            "\"name\": \"persian swordsman spell\",\n" +
+            "\"requiredManas\": 9,\n" +
+            "\"price\": 0\n" +
+            "},\n" +
+            "\"specialPowerActivationType\": \"ON_ATTACK\",\n" +
+            "\"positiveBuffs\": [],\n" +
+            "\"negativeBuffs\": [],\n" +
+            "\"hasFlag\": false,\n" +
+            "\"id\": \"\",\n" +
+            "\"name\": \"persian champion\",\n" +
+            "\"requiredManas\": 9,\n" +
+            "\"price\": 600\n" +
+            "}";
+    String persianCommander = "";
+    String transoxanianArcher = "";
+    String transoxanianSlingman = "";
+    String transoxaninaSpearman = "";
+    String transoxanianSpy = "";
+    String transoxanianMaceman = "";
+    String transoxanianPrince = "";
+    String blackDevil = "";
+    String stonemanGiant = "";
+    String eagle = "";
+    String swineriderDevil = "";
+    String monoeyedGiant = "";
+    String poisonousSnake = "";
+    String fieryDragon = "";
+    String predatorLion = "";
+    String giantSnake = "";
+    String whiteWolf = "";
+    String panther = "";
+    String wolf = "";
+    String witch = "";
+    String grandWitch = "";
+    String genii = "";
+    String wildSwine = "";
+    String elder = "";
+    String geeve = "";
+    String bahman = "";
+    String ashkboos = "";
+    String iraj = "";
+    String bigGiant = "";
+    String twoHeadedGiant = "";
+    String queenOfWinter = "";
+    String steelArmored = "";
+    String siavash = "";
+    String giantKing = "";
+    String arzhangDeeve = "";
+
 
 }
